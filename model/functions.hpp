e00153c467ac7b9e791d44363fb7dce3d3b7be71
Jakob Leben <jakob.leben@gmail.com>
2014-04-28 21:01:01 -0700

    functions: add averaging filter

--- model/functions.hpp
+++ model/functions.hpp
@@ -51,6 +51,59 @@ public:
     }
 };
 
+class avg_filter : public function
+{
+    int width;
+
+public:
+    avg_filter(int width = 3): width(width)
+    { assert(width > 0); }
+
+    int input_count() { return 1; }
+    int output_count()  { return 1; }
+    extent input_size( int index ) { return {1,width}; }
+    extent output_size( int index ) { return {1}; }
+
+    void generate( const stream_code::values & inputs,
+                   stream_code::values & outputs,
+                   stream_code::context & ctx )
+    {
+        using stream_code::endl;
+
+        assert(inputs.size() == 1);
+        assert(outputs.size() == 1);
+
+        string index = inputs[0].index[1];
+        stream_code::values local_inputs = inputs;
+        ostringstream code;
+
+        code << "(";
+
+        code << *local_inputs[0];
+
+        for (int i = 1; i < width; ++i)
+        {
+            ostringstream next_index;
+            next_index << index << " + " << (i+1);
+            local_inputs[0].index[1] = next_index.str();
+
+            code << " + " << *local_inputs[0];
+        }
+
+        code << ") / " << width;
+
+        stream_code::value & out = outputs[0];
+        if ( !out.id.empty() )
+        {
+            ctx << *out << " = " << code.str() << ";" << endl;
+        }
+        else
+        {
+            out.id = code.str();
+        }
+    }
+};
+
 } // namespace
 
 #endif // STREAM_FUNCTIONS_INCLUDED
